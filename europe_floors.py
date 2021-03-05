4b4379b211aaba901d73f771916adc4137a4d960
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-05 12:22:44 +0100

    Create europe_floors.py
    Use input() and int() to print US floor conversion from user's european floor

---
+++ europe_floors.py
@@ -0,0 +1,4 @@
+#Receive european floor number, and convert it to american floors by adding 1
+question = input("What is the European floor number?")
+floor= int(question) + 1
+print("The US floor is", floor)
