a4f55c410516ff6cef7be857089a07fb96f7088d
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-07 13:10:11 +0100

    Update gross_pay.py
    Add if else conditional code to add overtime pay for any hours worked over 40

--- gross_pay.py
+++ gross_pay.py
@@ -1,4 +1,12 @@
 hours = input("Enter Hours Worked:")
 rate = input("Enter Pay Per Hour:")
-pay= float(rate)*float(hours)
-print("Your Pay is", pay)
+fl_hours=float(hours) #convert the inputs to floats so you can use arithmatic operators
+fl_rate=float(rate)
+
+if fl_hours>40: #half rate overtime pay added for hours above 40 
+    overtime= (fl_hours-40) * (fl_rate*0.5)
+    pay= float(overtime) + (fl_rate*fl_hours)
+else: #or just rate multiplied by hours
+    pay= fl_rate*fl_hours
+
+print("Your Pay is", pay) #print answer
