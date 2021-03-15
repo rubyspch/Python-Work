900156a9246c438fb42826dee197f62a5f071bbd
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-15 22:03:13 +0100

    Create index_arrays.py
    Print different elements of array a

---
+++ numpy-practice/index_arrays.py
@@ -0,0 +1,5 @@
+a = np.array(['A','B','C','D','E'])
+print(a[0]) #prints first element
+print(a[4]) #prints fifth element
+print(a[:3]) #prints first 3 elements
+print(a[1:4]) #prints middle elements
