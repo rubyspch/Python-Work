41f63f082272e69e9ce3dd8f586ec2e9d69f3dd6
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-15 18:12:32 +0100

    Update create_arrays.py
    Check shape of matrix c using shape

--- numpy-practice/create_arrays.py
+++ numpy-practice/create_arrays.py
@@ -5,3 +5,6 @@ print(a.size) # prints number of elements in array
 b=np.arange(10,50) #adds numbers 10-49 to array
 print(b) 
 
+c=np.array([[1,1],[1,1]])
+print(c.shape) #outputs shape of matrix (2,2)
+
