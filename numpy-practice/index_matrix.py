3995eb8a94b228ed495b16c313dce7bb1a56c350
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-15 22:12:25 +0100

    Update index_matrix.py
    Prints specific elements of specific rows

--- numpy-practice/index_matrix.py
+++ numpy-practice/index_matrix.py
@@ -10,3 +10,6 @@ print(X[0]) #prints first row
 print(X[3]) #prints last row 
 print(X[0][0]) #prints first element on first row
 print(X[3][3]) #prints last element on last row
+print(X[1][1:3], X[2][1:3]) #Prints middle elements of middle rows
+print(X[0][:2], X[1][:2]) #prints first two elements of first two rows
+print(X[2][2:], X[3][2:]) #prints last two elements on last two rows
