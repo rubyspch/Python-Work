abe522d89c1ff5e0d52bb1245cb5b77c4b515275
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-16 15:05:51 +0100

    Update panda-work.py
    Add name to series Y and print values

--- pandas-practice/panda-work.py
+++ pandas-practice/panda-work.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 pd.Series([], dtype='float')
 
-X = ['A','B','C']
+X = ['A','B','C'] #create python list
 print(X, type(X))
-Y = pd.Series(X)
+Y = pd.Series(X) #convert python list X to pandas series Y
 print(Y, type(Y))
+Y.name='My letters'
+print(Y.values)
