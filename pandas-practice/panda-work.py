ccf4c77bd8c30b7f937f5c935c5b0dc97eaadbfa
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-16 15:25:56 +0100

    Update panda-work.py
    Create series Z.
    
    prints Z elements and print elements in reversed order

--- pandas-practice/panda-work.py
+++ pandas-practice/panda-work.py
@@ -13,4 +13,10 @@ print(Y.values)
 index_names = ['first', 'second', 'third'] 
 Y.index=index_names #adds list of strings to series as index names
 print(Y['first']) #prints element with index first
-print(Y['third']) #prints element with index third 
+print(Y['third']) #prints element with index third
+
+Z = pd.Series(['A','B','C','D','E'],
+              index=['first','second','third','fourth','fifth']) #creates new series Z with specified index names
+print(Z['second':'fourth']) #prints middle elements
+print(Z[['first','fifth']]) #prints first and fifth elements
+print(Z.sort_values(ascending=False)) #prints values in reverse order
