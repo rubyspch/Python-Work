67b29db00b55d82c1865aeac4932dfd4eebec9c1
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-16 12:53:22 +0100

    Update manipulate_numpy.py
    Create array c with np.zeros.
    
    Print using string literals and change an element using indexing

--- numpy-practice/manipulate_numpy.py
+++ numpy-practice/manipulate_numpy.py
@@ -5,3 +5,8 @@ a= np.asfarray(a) #changes element types to floats
 
 b = [-5, -3, 0, 10, 40]
 b=np.flip(b) #reverses order of b
+
+c = np.zeros(10) #creates array of zeros
+print(f'Before: {c}') #outputs array
+c[4]=1 #changes fifth element
+print(f'After: {c}') #outputs changed array
