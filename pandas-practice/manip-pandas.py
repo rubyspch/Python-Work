085c7af63132cbde902f92ee9a777807a7b9f896
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-16 16:11:40 +0100

    Update manip-pandas.py
    Change and add to elements in X

--- pandas-practice/manip-pandas.py
+++ pandas-practice/manip-pandas.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
 
-X = pd.Series([1,2,3,4,5],
-              index=['forth','second','fifth','first','third'], dtype='int64')
+X = pd.Series([4,2,5,1,3],
+              index=['fourth','second','fifth','first','third'], dtype='int64')
 print(X)
-X.sort_values(ascending=True)
+X=X.sort_values() #sorts values to ascending order
+print(X)
 X=X.astype('float64') #converts X elements to floats
+X[4]=10 #sets fifth element to 10
 print(X)
-
+X['second':'fourth']=0 #sets middle values to 0
+print(X)
+X+=5 #adds 5 to all elements
+print(X)
