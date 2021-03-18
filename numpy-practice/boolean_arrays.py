7d6fa338dfe37e2cf5cb3d909965184a35a333e1
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-18 19:38:52 +0100

    Update boolean_arrays.py
    Print c values that are greater than 5

--- numpy-practice/boolean_arrays.py
+++ numpy-practice/boolean_arrays.py
@@ -2,5 +2,8 @@ import numpy as np
 
 a = np.array([-1,2,0,-4,5,6,0,0,-9,10])
 
-b= a[a<0]
+b= a[a<0] #assigns elements in a that are less than 0 to b
 print(b)
+
+c = np.array([-1, 2, 0, -4, 5, 6, 0, 0, -9, 10])
+print(c[c>5]) #prints values in c that are greater than 5
