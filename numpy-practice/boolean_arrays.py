fdd06cd914470c4e1e0198f6e3613384bd9049fc
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-18 19:39:39 +0100

    Update boolean_arrays.py
    Print mean of Series d

--- numpy-practice/boolean_arrays.py
+++ numpy-practice/boolean_arrays.py
@@ -7,3 +7,6 @@ print(b)
 
 c = np.array([-1, 2, 0, -4, 5, 6, 0, 0, -9, 10])
 print(c[c>5]) #prints values in c that are greater than 5
+
+d = np.array([-1, 2, 0, -4, 5, 6, 0, 0, -9, 10])
+print(d[d>d.mean()])
