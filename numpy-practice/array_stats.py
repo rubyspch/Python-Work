9a257884f8787733086e7b7b9dd657ef429ec3aa
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-18 19:48:13 +0100

    Update array_stats.py
    print sums of Y columns

--- numpy-practice/array_stats.py
+++ numpy-practice/array_stats.py
@@ -5,3 +5,12 @@ X = np.array([3, 5, 6, 7, 2, 3, 4, 9, 4])
 print(sum(X)) #prints sum
 print(X.sum()) #prints sum
 print(X.mean()) #prints mean
+
+Y = np.array([
+    [1,   2,  3,  4],
+    [5,   6,  7,  8],
+    [9,  10, 11, 12],
+    [13, 14, 15, 16]
+])
+
+print(np.sum(Y, axis=0)) #prints sums of columns (axis 0)
