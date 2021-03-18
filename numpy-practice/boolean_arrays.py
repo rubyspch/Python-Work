60e6c8f5af216acf5357e374cb0d644afef68d0f
rubyspch <ruby_chohan@hotmail.co.uk>
2021-03-18 19:42:29 +0100

    Update boolean_arrays.py
    Apply code to one Series a

--- numpy-practice/boolean_arrays.py
+++ numpy-practice/boolean_arrays.py
@@ -5,8 +5,10 @@ a = np.array([-1,2,0,-4,5,6,0,0,-9,10])
 b= a[a<0] #assigns elements in a that are less than 0 to b
 print(b)
 
-c = np.array([-1, 2, 0, -4, 5, 6, 0, 0, -9, 10])
-print(c[c>5]) #prints values in c that are greater than 5
+print(a[a>5]) #prints values in a that are greater than 5
 
-d = np.array([-1, 2, 0, -4, 5, 6, 0, 0, -9, 10])
-print(d[d>d.mean()]) #prints mean of d
+print(a[a>a.mean()]) #prints mean of a
+
+print(a[(a==2)|(a==10)]) #prints elements equal to 2 or 10
+
+
