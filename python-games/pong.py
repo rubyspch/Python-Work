f4a7cf11be4a43e10ec2079702d1cd7355e1e6a7
rubyspch <ruby_chohan@hotmail.co.uk>
2021-05-09 11:53:11 +0200

    Create pong.py
    Create pong.py
    
    Import turtle and create wn

---
+++ python-games/pong.py
@@ -0,0 +1,14 @@
+import turtle #built in module for beginners games 
+
+wn = turtle.Screen() #creates a window
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0) #stops windows from updating, have to manually update them
+#Main game loop
+
+#Paddle A
+
+#Paddle B
+
+#Ball
